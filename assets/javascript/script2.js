d3a0b32c76c5792cedbbfa3665ba284292e7279e
jkrowden <jkrowden@gmail.com>
2018-03-13 21:02:49 -0600

    Added

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -21,23 +21,17 @@ $(document).ready(function () {
 			// adding firebase items to variables
 			foodItem = snapshot.val().ingredients;
 			foodId = snapshot.val().title;
-			console.log("line 24 snap", snapshot.val().ingredients);
-			console.log(foodItem);
-			console.log(foodId);
 			appendItems();
 		});
 		// onclick food items to pull from api
 		$(document).on("click", ".arrayItems", function () {
-
 			var loadingImg = $("<div>");
 			loadingImg.text('loading...');
 			$("#addedItems").append(loadingImg);
 
 			var inputItem = $(this).text();
 			// api url pulling from food only queries
-			var queryURL = "http://api.walmartlabs.com/v1/search?query=" + inputItem + "&format=json&apiKey=t4a2y6c96t4m3mffyc4fmvcc&categoryId=976759";
-
-			console.log(queryURL);
+			var queryURL = "https://api.walmartlabs.com/v1/search?query=" + inputItem + "&format=json&apiKey=t4a2y6c96t4m3mffyc4fmvcc&categoryId=976759";
 			$.ajax({
 				url: queryURL,
 				method: "GET",
@@ -47,12 +41,15 @@ $(document).ready(function () {
 				// adding 10 results from the api in this variable
 				var results = response.items;
 				// loop through all 10 items to grab info individualy
+				console.log(results)
 				for (var i = 0; i < results.length; i++) {
 					var price = results[i].salePrice;
 					var foodName = results[i].name;
 					var thumbNail = results[i].thumbnailImage;
+					var itemId = results[i].itemId;
+
 					// running function to add items and their attributes to the page
-					addItem(thumbNail, foodName, price);
+					addItem(thumbNail, foodName, price, itemId);
 				}
 			}
 
@@ -62,20 +59,17 @@ $(document).ready(function () {
 	// function to add items and their attributes to the page
 	findingItems();
 
-	function addItem(thumbNail, foodName, price) {
-		$("#addedItems").append("<tr><td><img src='" + thumbNail + "' /></td><td>" + foodName + "</td><td>" + price + "</td></tr><br>");
+	function addItem(thumbNail, foodName, price, itemId) {
+		$("#addedItems").append("<tr class = 'foodName' data-itemid=" + itemId + "><td><img src='" + thumbNail + "' /></td><td>" + foodName + "</td><td>" + price + "</td></tr>");
 	};
-
 	function appendItems() {
 		for (var l = 0; l < foodItem.length; l++) {
-			console.log("food Items", foodItem[l]);
 			$("#recipeList").append("<p class='arrayItems'>" + foodItem[l] + "</p>");
 		};
 	};
 });
 
-
-// add title to recipe
-// add hover color to the items to select on the cernter console
-// add items to list colume
-// add button to purchase items at walmart
+$(document).on("click", ".foodName", function () {
+	foodName = this;
+	$("#addedList").append(foodName);
+});
